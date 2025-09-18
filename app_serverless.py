4e0b8c2a6c82e1e5a82064569e26f6ab91419c3e
Anil Kumar <anil29717@gmail.com>
2025-09-18 12:29:00 +0530

    Add Render deployment configuration

--- app_serverless.py
+++ app_serverless.py
@@ -216,4 +216,5 @@ def admin_analytics():
                          journeys=user_journeys)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
